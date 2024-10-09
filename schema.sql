6ea55bfdf9c0f6ca9083f7a93e3b0e8a1d7b991e
Rayhan Rizqi <97212180+RayhanRizqi@users.noreply.github.com>
2024-10-09 12:54:56 -0700

    commented schema.sql

--- schema.sql
+++ schema.sql
@@ -9,43 +9,43 @@ DROP TABLE IF EXISTS library;
 
 
 CREATE TABLE users (
-    uid SERIAL PRIMARY KEY, 
-    name TEXT NOT NULL, 
-    hash TEXT NOT NULL, 
+    uid SERIAL PRIMARY KEY, -- user id
+    name TEXT NOT NULL, -- user's name
+    hash TEXT NOT NULL, -- user's password stored after encryption using SHA256
     membership_id TEXT[], -- memberships the user has
     bids TEXT[] -- Books the user has checked out
 );
 
 CREATE TABLE library (
-                    lid SERIAL PRIMARY KEY,
+                    lid SERIAL PRIMARY KEY, -- library id
                     bids TEXT[], -- books available in the library
-                    membership_id TEXT
+                    membership_id TEXT -- the prefix of a member's membership id to this library
 );
 
 CREATE TABLE book (
-    bid TEXT PRIMARY KEY,
-    title TEXT NOT NULL,
-    author TEXT NOT NULL,
-    year INTEGER NOT NULL,
-    book_cover TEXT NOT NULL
+    bid TEXT PRIMARY KEY, -- book id
+    title TEXT NOT NULL, -- book title
+    author TEXT NOT NULL, -- book's author
+    year INTEGER NOT NULL, -- publishing year
+    book_cover TEXT NOT NULL -- book's front cover in the form of a url link
 );
 
 CREATE TABLE membership (
-    lid INTEGER NOT NULL,
-    uid INTEGER NOT NULL,
-    date_of_acquisition DATE,
+    lid INTEGER NOT NULL, -- foreign key for the library id
+    uid INTEGER NOT NULL, -- foreign key for the user id
+    date_of_acquisition DATE, -- the date that the user got the membership
     PRIMARY KEY (lid, uid),
     FOREIGN KEY (lid) REFERENCES library (lid),
     FOREIGN KEY (uid) REFERENCES users (uid)
 );
 
 CREATE TABLE checkBook (
-    lid INTEGER NOT NULL,
-    uid INTEGER NOT NULL,
-    bid TEXT NOT NULL,
-    checkIn BOOLEAN,
-    checkOutDate DATE,
-    duration INTEGER,
+    lid INTEGER NOT NULL, -- foreign key for the library id
+    uid INTEGER NOT NULL, -- foreign key for the user id
+    bid TEXT NOT NULL, -- foreign key for the book id
+    checkIn BOOLEAN, -- True if the book has been checked back into the library
+    checkOutDate DATE, -- the date the book got checked out
+    duration INTEGER, -- the number of days before the user has to return the book
     PRIMARY KEY (lid, uid, bid),
     FOREIGN KEY (lid) REFERENCES library (lid),
     FOREIGN KEY (uid) REFERENCES users (uid),
